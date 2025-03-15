2d69f74ab6ea1f729d7c857e4218b4abaffd2bb6
wataru11111 <setowataru1771@outlook.jp>
2025-03-16 02:08:54 +0900

    管理者側でお休み確認修正

--- app/controllers/admin/offs_controller.rb
+++ app/controllers/admin/offs_controller.rb
@@ -1,14 +1,12 @@
 class Admin::OffsController < ApplicationController
   def index
     if params[:search_date].present?
-      begin
-        search_date = Date.parse(params[:search_date].tr("/", "-")) # "/"を"-"に統一し、Date型に変換
-        @offs = Off.where("DATE(off_month) = ?", search_date) # DATE関数を使って検索
-      rescue ArgumentError
-        @offs = [] # 日付のパースに失敗した場合は空配列を返す
-      end
+      # search_date を Date 型に変換
+      search_date = Date.strptime(params[:search_date], "%Y-%m-%d") rescue nil
+      @offs = search_date ? Off.where(off_month: search_date) : Off.none
     else
       @offs = Off.all
     end
   end
 end
+
