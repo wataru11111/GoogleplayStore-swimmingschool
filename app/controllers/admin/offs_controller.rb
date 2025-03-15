7c412dbe1041ef424883185db659b1f32ddacc8b
wataru11111 <setowataru1771@outlook.jp>
2025-03-15 15:09:34 +0900

    GoogleplayStore-swimmingschool に移行

--- app/controllers/admin/offs_controller.rb
+++ app/controllers/admin/offs_controller.rb
@@ -1,8 +1,12 @@
 class Admin::OffsController < ApplicationController
   def index
     if params[:search_date].present?
-      search_date = Date.parse(params[:search_date])
-      @offs = Off.where(off_month: search_date)
+      begin
+        search_date = Date.parse(params[:search_date].tr("/", "-")) # "/"を"-"に統一し、Date型に変換
+        @offs = Off.where("DATE(off_month) = ?", search_date) # DATE関数を使って検索
+      rescue ArgumentError
+        @offs = [] # 日付のパースに失敗した場合は空配列を返す
+      end
     else
       @offs = Off.all
     end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,6 @@
 const { environment } = require('@rails/webpacker')
 
+// ↓ 不要な node 設定を削除または修正
+environment.config.set('node', false);
+
 module.exports = environment
