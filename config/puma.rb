0b0ec6af2bb28f08aa86abc7f284b3dcddeb6c66
wataru11111 <setowataru1771@outlook.jp>
2025-05-03 12:47:31 +0900

    config/puma.rbを変更

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,4 @@
 # Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
@@ -7,8 +6,8 @@ threads min_threads_count, max_threads_count
 # Worker timeout in development
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-# Default port to listen on
-port ENV.fetch("PORT", 8080)
+# ✅ RailwayやDockerで使う TCPバインド方式（これだけでOK）
+bind "tcp://0.0.0.0:#{ENV.fetch("PORT", 8080)}"
 
 # The environment Puma will run in
 environment ENV.fetch("RAILS_ENV", "production")
@@ -18,7 +17,4 @@ pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
 
 # Allow puma to be restarted by `rails restart` command
 plugin :tmp_restart
-
-# Use TCP binding (Railway/Docker 向け)
-bind "tcp://0.0.0.0:#{ENV.fetch("PORT", 8080)}"
 
