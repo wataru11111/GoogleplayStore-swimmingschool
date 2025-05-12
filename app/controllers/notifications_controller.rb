8e5ef4c5a991f880dc4de76c45f6194cf4e1b39a
wataru11111 <setowataru1771@outlook.jp>
2025-05-12 16:25:02 +0900

    お知らせエラー更新

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,17 +1,17 @@
 class NotificationsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def update_notice
-  Rails.logger.info "📥 受信したAuthorizationヘッダー: #{request.headers['Authorization']}"
-  expected = "Bearer #{ENV['INTERNAL_NOTIFY_TOKEN']}"
-  Rails.logger.info "🔐 比較対象: #{expected}"
+   def update_notice
+   Rails.logger.info "📥 受信したAuthorizationヘッダー: #{request.headers['Authorization']}"
+   expected = "Bearer #{ENV['INTERNAL_NOTIFY_TOKEN']}"
+   Rails.logger.info "🔐 比較対象: #{expected}"
 
-  return head :unauthorized unless request.headers['Authorization'] == expected
+   return head :unauthorized unless request.headers['Authorization'] == expected
 
-  title = params[:title] || "お知らせが更新されました"
-  message = params[:message] || "新しいスケジュールが公開されました"
-  OneSignalService.send_notification(title: title, message: message)
+   title = params[:title] || "お知らせが更新されました"
+   message = params[:message] || "新しいスケジュールが公開されました"
+   OneSignalService.send_notification(title: title, message: message)
 
-  head :ok
+   head :ok
+ end
 end
-
