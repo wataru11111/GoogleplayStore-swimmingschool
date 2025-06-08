4094caecc358c5fe3c0ff8b3a79e1cad5a8a5259
wataru11111 <setowataru1771@outlook.jp>
2025-06-09 00:51:16 +0900

    パスワードリセット完成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -18,5 +18,5 @@ end
 
 
  def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :password, :password_confirmation)
  end
