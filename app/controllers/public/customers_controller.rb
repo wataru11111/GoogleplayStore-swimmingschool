b0feb795c71f43fc4c2bee9c8a881e30ae5a7eef
wataru11111 <setowataru1771@outlook.jp>
2025-06-14 18:39:12 +0900

    ユーザー情報更新完成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -10,13 +10,29 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
-    @customer = current_customer
-    @customer.update(customer_params)
-    redirect_to customers_show_path
+  @customer = current_customer
+  if @customer.update(customer_params)
+    redirect_to customers_show_path, notice: "ユーザー情報を更新しました。"
+  else
+    flash.now[:alert] = "入力内容に誤りがあります。"
+    render :edit
   end
 end
 
 
+ 
  def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :password, :password_confirmation)
+  permitted = params.require(:customer).permit(
+    :last_name, :first_name, :last_name_kana, :first_name_kana,
+    :email, :postal_code, :address, :telephone_number,
+    :password, :password_confirmation
+  )
+
+  if permitted[:password].blank?
+    permitted.delete(:password)
+    permitted.delete(:password_confirmation)
+  end
+
+  permitted
  end
+end
